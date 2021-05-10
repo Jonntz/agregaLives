4e92138285f20adac9a62cd198be1e4f8e31bbbe
Jonntz <65257445+Jonntz@users.noreply.github.com>
2021-05-09 21:31:08 -0300

    Realizando integração com a API e buscando os dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { HomeComponent } from './views/home/home.component';
+import { LiveListComponent } from './views/home/live-list/live-list.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatTabsModule} from '@angular/material/tabs';
-import { LiveListComponent } from './views/home/live-list/live-list.component';
 
 @NgModule({
-  declarations: [
+  declarations:[
     AppComponent,
     HomeComponent,
     LiveListComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule,
+     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatTabsModule
+     MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
